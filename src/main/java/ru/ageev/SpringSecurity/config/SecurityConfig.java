664dc590c06a390a5fb911b153dc739b0ccd61f6
tmrke <namat9@yandex.ru>
2023-11-08 21:50:42 +0700

    update to spring security version 6

--- src/main/java/ru/ageev/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/ru/ageev/SpringSecurity/config/SecurityConfig.java
@@ -2,17 +2,20 @@ package ru.ageev.SpringSecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.ageev.SpringSecurity.service.PersonDetailsService;
 
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
     private final PersonDetailsService personDetailsService;
 
     @Autowired
@@ -21,9 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
-    }
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(personDetailsService);
+//    }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
--- src/main/java/ru/ageev/SpringSecurity/models/Person.java
+++ src/main/java/ru/ageev/SpringSecurity/models/Person.java
@@ -1,9 +1,11 @@
 package ru.ageev.SpringSecurity.models;
 
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 import java.sql.Date;
 
 @Entity
