182150bbc1319db7698c59097e37d49115acc3dd
tmrke <namat9@yandex.ru>
2023-11-08 23:19:02 +0700

    added SecurityFilterChain

--- src/main/java/ru/ageev/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/ru/ageev/SpringSecurity/config/SecurityConfig.java
@@ -3,18 +3,22 @@ package ru.ageev.SpringSecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.ageev.SpringSecurity.service.PersonDetailsService;
 
 
 @Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final PersonDetailsService personDetailsService;
 
@@ -23,10 +27,25 @@ public class SecurityConfig {
         this.personDetailsService = personDetailsService;
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        (auth) -> auth.requestMatchers("/auth/login", "/error", "/auth/login_error").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(
+                        (form) -> form.
+                                loginPage("/auth/login")
+                                .loginProcessingUrl("/process_login")
+                                .defaultSuccessUrl("/person", true)
+                                .failureUrl("/auth/login?error")
+                );
 
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(personDetailsService);
-//    }
+        return http.build();
+    }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
